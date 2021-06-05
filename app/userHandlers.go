3e73dbf1b51cb439d9bed873531d2e01fba12b68
Daniel Davidson <dan@bitmono.com>
2021-06-05 14:20:00 +0100

    Add method to get user and wrtie JSON reponse

--- app/userHandlers.go
+++ app/userHandlers.go
@@ -1,7 +1,32 @@
 package app
 
-import "github.com/da-n/portfolio-app/service"
+import (
+	"encoding/json"
+	"github.com/da-n/portfolio-app/service"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 type UserHandlers struct {
 	Service service.UserService
+}
+
+func (h UserHandlers) GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["user_id"]
+	user, err := h.Service.GetUser(userId)
+	if err != nil {
+		writeJsonResponse(w, err.Code, err.Message)
+	} else {
+		writeJsonResponse(w, http.StatusOK, user)
+	}
+}
+
+func writeJsonResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
 }
