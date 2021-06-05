c15cc8ad375073adf184baa596ae1465a4e39f41
Daniel Davidson <dan@bitmono.com>
2021-06-05 13:16:47 +0100

    Add DB client, and start wiring hex architecture

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/da-n/portfolio-app/app"
+	"github.com/da-n/portfolio-app/domain"
+	"github.com/da-n/portfolio-app/service"
+	"github.com/jmoiron/sqlx"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	// check that all env vars are set
 	if err := envCheck(); err != nil {
 		log.Fatal(err)
 	}
 
+	// get a mysql client instance
+	dbClient, err := getDbClient()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// create database repositories
+	userRepositoryDb := domain.NewUserRepositoryDb(dbClient)
+
+	// create handlers
+	userHandlers := app.UserHandlers{service.NewUserService(userRepositoryDb)}
 }
 
 // envCheck verify all necessary environment variables are set to run application
@@ -19,6 +35,11 @@ func envCheck() error {
 	envVars := []string{
 		"SERVER_ADDRESS",
 		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWORD",
+		"DB_ADDRESS",
+		"DB_PORT",
+		"DB_NAME",
 	}
 
 	for _, k := range envVars {
@@ -30,3 +51,23 @@ func envCheck() error {
 	return nil
 }
 
+// getDbClient create a new database client using sqlx
+func getDbClient() (*sqlx.DB, error) {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbAddress := os.Getenv("DB_ADDRESS")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbAddress, dbPort, dbName)
+	client, err := sqlx.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+
+	return client, nil
+}
--- main_test.go
+++ main_test.go
@@ -19,6 +19,11 @@ func Test_it_should_return_nil_when_env_vars_have_been_set(t *testing.T) {
 	// given
 	os.Setenv("SERVER_ADDRESS", "localhost")
 	os.Setenv("SERVER_PORT", "8080")
+	os.Setenv("DB_USER", "root")
+	os.Setenv("DB_PASSWORD", "badpassword123")
+	os.Setenv("DB_ADDRESS", "localhost")
+	os.Setenv("DB_PORT", "3306")
+	os.Setenv("DB_NAME", "portfolio-app")
 
 	// when
 	err := envCheck()
