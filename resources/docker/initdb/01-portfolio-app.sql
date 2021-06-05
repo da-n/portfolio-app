0a6c7a6007b1140bcf3ba8e379716c17ba5d8e66
Daniel Davidson <dan@bitmono.com>
2021-06-05 15:23:13 +0100

    Add dump file for seeding MySQL

---
+++ resources/docker/initdb/01-portfolio-app.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: portfolio_app
+-- ------------------------------------------------------
+-- Server version	5.6.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+CREATE DATABASE portfolio_app;
+USE portfolio_app;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+                             `user_id` int(11) NOT NULL AUTO_INCREMENT,
+                             `first_name` varchar(191) NOT NULL,
+                             `last_name` varchar(191) NOT NULL,
+                             `email` varchar(191) NOT NULL,
+                             `password` varchar(191) NOT NULL,
+                             PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(1,'Theia','Parker','theia@example.com','password123'),
+(2,'Kaine','Berger','kaine@example.com','password123'),
+(3,'Rami','Mejia','rami@example.com','password123'),
+(4,'Junayd','Bishop','junayd@example.com','password123'),
+(5,'Alyce','Blake','alyce@example.com','password123'),
+(6,'Callum','Hussain','callum@example.com','password123');
+
+
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-31 10:25:19
