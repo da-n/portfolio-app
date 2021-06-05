ca18713cea5ed0feef213f51897f32ca4809e13f
Daniel Davidson <dan@bitmono.com>
2021-06-05 14:20:45 +0100

    Use custom errors to allow response codes and message

--- service/userService.go
+++ service/userService.go
@@ -3,20 +3,21 @@ package service
 import (
 	"github.com/da-n/portfolio-app/domain"
 	"github.com/da-n/portfolio-app/dto"
+	"github.com/da-n/portfolio-app/errs"
 )
 
 type UserService interface {
-	GetUser(string) (*dto.UserResponse, *error)
+	GetUser(string) (*dto.UserResponse, *errs.AppError)
 }
 
 type DefaultUserService struct {
 	Repo domain.UserRepository
 }
 
-func (service DefaultUserService) GetUser(id string) (*dto.UserResponse, *error) {
+func (service DefaultUserService) GetUser(id string) (*dto.UserResponse, *errs.AppError) {
 	return nil, nil
 }
 
-func NewUserService(repo domain.UserRepository) DefaultUserService {
-	return DefaultUserService{repo}
+func NewUserService(r domain.UserRepository) DefaultUserService {
+	return DefaultUserService{r}
 }
